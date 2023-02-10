446f32f55b7de7318b57c1cd9d761d639fe8d391
Akash L M <91725049+AKASH-L-M@users.noreply.github.com>
2023-02-10 14:41:48 +0530

    Update script.js
    added timeDifference function definition

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -58,14 +58,24 @@ var countdownfunction = setInterval(function() {
   var today = hour + ":" + minute + ":" + second;
 
   document.getElementById("timer").innerHTML = today;
-  document.getElementById("timediff").innerHTML = today;
-  
-
  
 }, 1000);
 
-   
 
+var timeDifference = setInterval(function() {
+  var d = new Date().toJSON().slice(11, 19);
+
+  var cStart = d;
+  var cStop = "12:10:15";
+
+  if (cStart != "" && cStop != "") {
+      var tStart = parseTime(cStart);
+      var tStop = parseTime(cStop);
+
+      var today = "Minutes:"+(tStop - tStart)/(1000*60);
+  }
+   document.getElementById("timediff").innerHTML = today;
+}, 1000);
 /**
  * header sticky & go to top
  */
