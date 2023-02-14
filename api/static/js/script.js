c0c946f942d35ed5ae230c4724313590c6303f86
Akash L M <91725049+AKASH-L-M@users.noreply.github.com>
2023-02-14 13:28:01 +0530

    Update script.js
    added countdownfunction

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -53,31 +53,28 @@ window.addEventListener("scroll", function () {
 /**
  * Function definition for displaying live time on the screen
  */
-function currentTime() {
-  let date = new Date(); 
-  let hh = date.getHours();
-  let mm = date.getMinutes();
-  let ss = date.getSeconds();
-  let session = "AM";
+/**
+ * timer function
+ */
 
-  if(hh === 0){
-      hh = 12;
-  }
-  if(hh > 12){
-      hh = hh - 12;
-      session = "PM";
-   }
+var countdownfunction = setInterval(function() {
 
-   hh = (hh < 10) ? "0" + hh : hh;
-   mm = (mm < 10) ? "0" + mm : mm;
-   ss = (ss < 10) ? "0" + ss : ss;
-    
-   let time = hh + ":" + mm + ":" + ss + " " + session;
+  var d = new Date();
+  var currentOffset = d.getTimezoneOffset();
+  var ISTOffset = 330;   // IST offset UTC +5:30 
+  var ISTTime = new Date(d.getTime() + (ISTOffset + currentOffset)*60000);
 
-  document.getElementById("clock").innerText = time; 
-  let t = setTimeout(function(){ currentTime() }, 1000);
-}
+  var hour =  ISTTime.getHours();
+  var minute =  ISTTime.getMinutes();
+  var second =  ISTTime.getSeconds();
 
-currentTime();
+  if (hour.toString().length < 2) hour = '0' + hour;
+  if (minute.toString().length < 2) minute = '0' + minute;
+  if (second.toString().length < 2) second = '0' + second;
 
+  var today = hour + ":" + minute + ":" + second;
+
+  document.getElementById("time").innerHTML = today;
+ 
+}, 1000);
    
