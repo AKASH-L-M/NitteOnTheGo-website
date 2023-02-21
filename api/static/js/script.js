feada40002be22ca5bc8c8ed03246ddbaf659cc2
Akash L M <91725049+AKASH-L-M@users.noreply.github.com>
2023-02-21 08:50:21 +0530

    updated javascript file
    The website is expected to work properly

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -51,6 +51,13 @@ window.addEventListener("scroll", function () {
 /**
  * timer function
  */
-
+`use strict`;
+    function refreshTime() {
+    const timeDisplay = document.getElementById("time");
+    const dateString = new Date().toLocaleString();
+    const formattedString = dateString.replace(", ", " - ");
+    timeDisplay.textContent = formattedString;
+  }
+    setInterval(refreshTime, 1000);
 
    
